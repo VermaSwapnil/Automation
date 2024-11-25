716cb568838c49f3ecdb4ac8f245c319ea138a26
Dell <Dell@DESKTOP-SV6O24V>
2024-11-25 21:47:00 +0530

    4th

--- com.itpl/src/test/java/com/itpl/ActionsClassTest.java
+++ com.itpl/src/test/java/com/itpl/ActionsClassTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class ActionsClass {
+public class ActionsClassTest {
 	
 	public static void main(String[] args) throws InterruptedException {
 		
